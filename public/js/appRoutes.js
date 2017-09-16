0e806539f3c07ff527a5d194f2d9407bb0959def
Ketan Naik <ketan.naik@mobiuso.com>
2017-09-16 19:13:58 +0530

    Slider added. 3 column template added

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -8,12 +8,12 @@ angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', f
 			controller: 'MainController'
 		})
 
-		.when('/nerds', {
+		.when('/international', {
 			templateUrl: 'views/nerd.html',
 			controller: 'NerdController'
 		})
 
-		.when('/geeks', {
+		.when('/domestic', {
 			templateUrl: 'views/geek.html',
 			controller: 'GeekController'	
 		});
